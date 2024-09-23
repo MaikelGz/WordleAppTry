66e0c7cd5e6b90de2641fc98a286d76c16d66574
Maikel <uo289630@uniovi.es>
2024-09-23 17:06:48 +0200

    Todo listo, faltaria pulir...

--- app/src/main/java/com/example/aciertapalabra/MainActivity.kt
+++ app/src/main/java/com/example/aciertapalabra/MainActivity.kt
@@ -14,14 +14,21 @@ class MainActivity : AppCompatActivity() {
     private lateinit var buttonEnviarNombre : Button
     private lateinit var editTextNombre : EditText
     private lateinit var textViewNombre : TextView
-    private var listaPalabras= listOf("Jesus", "Adios", "Roblox", "Garen")
+    private lateinit var textViewIntentos : TextView
+    private lateinit var textViewPremio : TextView
+
+    private var listaPalabras= listOf("Jesus", "Adios", "Roble", "Garen")
     private var palabra = ""
+    private var terminado= false
+    private var intentos=5
 
     /* Inicia los elementos buscandolos por su id */
     private fun iniciarElementos() {
         buttonEnviarNombre = findViewById(R.id.buttonEnviarNombre)
         editTextNombre = findViewById(R.id.escribeTuNombre)
         textViewNombre = findViewById(R.id.textViewPalabra)
+        textViewIntentos= findViewById(R.id.textViewIntentos)
+        textViewPremio= findViewById(R.id.textViewPremio)
     }
 
     private fun elegirPalabra() {
@@ -30,27 +37,49 @@ class MainActivity : AppCompatActivity() {
 
     private fun validar(){
         buttonEnviarNombre.setOnClickListener { view_ ->
-            if( editTextNombre.text.length<5){
-                Toast.makeText(this, "Tamaño inferior a 5 caracteres", Toast.LENGTH_SHORT).show()
-            }else{
-                Toast.makeText(this,"Boton pulsado", Toast.LENGTH_SHORT).show()
+            if(!terminado){
+                if( editTextNombre.text.length!=5 ){
+                    Toast.makeText(this, "Tamaño distinto a 5 caracteres", Toast.LENGTH_SHORT).show()
+                }else{
+                    Toast.makeText(this,"Validando...", Toast.LENGTH_SHORT).show()
+                    val intento = editTextNombre.text
+                    var num=0
+                    var newText = ""
 
-                val intento = editTextNombre.text
-                var num=0
-                var newText = ""
-
-                for (letra in palabra) {
-                    if(intento.getOrNull(num)==letra){
-                        newText+=letra+" "
+                    if(intento.toString()==palabra){
+                        textViewNombre.text = palabra
+                        terminado=true
+                        buttonEnviarNombre.isEnabled=false
+                        editTextNombre.text.clear()
+                        editTextNombre.isEnabled=false
+                        textViewPremio.visibility=TextView.VISIBLE
+                        intentos--
+                        textViewIntentos.text = intentos.toString() + " intentos"
                     }else{
-                        newText+="_ "
+                        for (letra in palabra) {
+                            if(intento.getOrNull(num)==letra){
+                                newText+=letra+" "
+                            }else{
+                                newText+="_ "
+                            }
+                            num++
+                        }
+                        intentos--
+                        if(intentos==0){
+                            terminado=true
+                            buttonEnviarNombre.isEnabled=false
+                            editTextNombre.isEnabled=false
+                        }
+                        textViewIntentos.text = intentos.toString() + " intentos"
+                        textViewNombre.text = newText
+                        editTextNombre.text.clear()
                     }
-                    num++
                 }
+            }else{
 
-                textViewNombre.text = newText
-                editTextNombre.text.clear()
             }
+
+
 
         }
     }
